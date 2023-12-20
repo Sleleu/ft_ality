442794b32c8df059551dfef6dfbbf51ed77a1cfb
Sebastien LELEU <leleu.sebastien@hotmail.fr>
2023-12-20 14:26:26 +0100

    begin parsing args and file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::fs;
+
+fn get_file(filename: &String)-> String {
+    fs::read_to_string(filename)
+        .expect("Error while reading file")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("yoyoyo");
+    }
+    println!("{:?} {:?}", args, args.len());
+    let file = get_file(&args[1]);
+    println!("File content:\n{}", file)
 }
