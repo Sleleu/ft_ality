d1dcb314ca9a6a376910e8ce478ae463f225b885
Sebastien LELEU <leleu.sebastien@hotmail.fr>
2023-12-21 17:03:52 +0100

    add print keys

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::io::{stdin, stdout, Write};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+use std::process::exit;
 use std::env;
 use std::fs;
 mod parsing;
@@ -7,6 +12,29 @@ fn get_file(filename: &String)-> String {
         .expect("Error while reading file")
 }
 
+fn print_keys(c: Key) -> bool {
+    match c {
+        Key::Char(c) => println!("{:?}\r", Key::Char(c)),
+        Key::Ctrl(c) => println!("{:?}\r", Key::Ctrl(c)),
+        Key::Esc => return true,
+        _ => (),
+    }
+    false
+}
+
+fn main_loop() {
+    let stdout = stdout().into_raw_mode().unwrap();
+    println!("Welcome !\n\r");
+
+    for c in stdin().keys() {
+        match c {
+            Ok(c) => if print_keys(c) {break},
+            Err(c) => (),
+        }
+    }
+    println!("End !\r");
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
@@ -14,4 +42,5 @@ fn main() {
     }
     let str_file: String = get_file(&args[1]);
     let (keymap, combos) = parsing::parse_file(&str_file);
-}
+    main_loop();
+    }
