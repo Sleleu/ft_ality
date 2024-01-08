cd7a909a2c17cc16ca8afcffc57bc009209c7992
Sebastien LELEU <leleu.sebastien@hotmail.fr>
2024-01-08 18:47:00 +0100

    begin parsing keymap

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use termion::raw::IntoRawMode;
 use std::process::exit;
 use std::env;
 use std::fs;
+use indexmap::IndexMap;
 mod parsing;
 
 fn get_file(filename: &String)-> String {
@@ -12,76 +13,17 @@ fn get_file(filename: &String)-> String {
         .expect("Error while reading file")
 }
 
-fn print_keys(c: Key) -> Result<(), ()> {
-    match c {
-        Key::Char(c) => println!("{:?}\r", c),
-        Key::Esc => return Err(()),
-        _ => (),
-    }
-    Ok(())
-}
-
-fn listen_moves() {
-    let stdout = stdout().into_raw_mode();
-    let result = stdin().keys().try_for_each(|c| { c.map_err(|_| ()).and_then(print_keys) });
-}
-
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 2 { panic!("Usage: cargo run [filename]"); }
+    if args.len() != 2 {
+        println!("Usage: cargo run [filename]"); exit(1)
+    }
     let str_file: String = get_file(&args[1]);
-    let (keymap, combos) = parsing::parse_file(&str_file);
-    listen_moves();
+    // dbg!(&str_file); // OK
+    parsing::parse_file(&str_file);
+    // let Ok((keymap, combos)) = parsing::parse_file(&str_file)
+    //     else {
+    //         println!("Parsing Error"); exit(1) 
+    //     };
+    // listen_moves(keymap, combos);
 }
-
-
-/* SLD2 TEST */
-// extern crate sdl2; 
-
-// use sdl2::event::Event;
-// use sdl2::keyboard::Keycode;
-// use sdl2::EventPump;
-// use std::env;
-// use std::fs;
-
-// mod parsing;
-
-// fn get_file(filename: &String) -> String {
-//     fs::read_to_string(filename)
-//         .expect("Error while reading file")
-// }
-
-// fn event_loop(event_pump: &mut sdl2::EventPump) {
-//     'running: loop {
-//         for event in event_pump.poll_iter() {
-//             match event {
-//                 Event::Quit {..} |
-//                 Event::KeyDown { keycode: Some(Keycode::Escape), .. } => break 'running,
-//                 Event::KeyDown { keycode: Some(key), .. } => println!("{:?}", key),
-//                 _ => {}
-//             }
-//         }
-//     }
-// }
-
-// fn init_sdl() {
-//     let sdl_context = sdl2::init().unwrap();
-//     let video_subsystem = sdl_context.video().unwrap();
-//     let window = video_subsystem
-//         .window("Ft_ality", 100, 100)
-//         .build()
-//         .expect("Could not initialize the video subsystem");
-
-//     let mut event_pump = sdl_context.event_pump().unwrap();
-//     event_loop(&mut event_pump)
-// }
-
-// fn main() {
-//     let args: Vec<String> = env::args().collect();
-//     if args.len() != 2 {
-//         panic!("Usage: cargo run [filename]");
-//     }
-//     let str_file: String = get_file(&args[1]);
-//     let (keymap, combos) = parsing::parse_file(&str_file);
-//     init_sdl();
-// }
--- src/parsing.rs
+++ src/parsing.rs
@@ -1,35 +1,41 @@
+use std::process::exit;
 use indexmap::IndexMap;
 
-// Parsing :
-//      - chaque fichier suit : <keymap>""<moves>, tout newline supplémentaire ou autre = erreur
-//      - chaque ligne doit contenir un séparateur ';', et contenir du texte avant et après
-//      - Tout move est composé des actions du keymapping, et peut contenir un '+' pour définir un ensemble de moves, sinon erreur
-//      - Aucun duplicata autorisé sur les K,V de keymap, ou K,V de moves
 
-fn get_keymap() -> IndexMap<String, String> {
-    [
-        ("d".to_string(), "right".to_string()),
-        ("a".to_string(), "left".to_string()),        
-        (" ".to_string(), "jump".to_string()),
-        ("j".to_string(), "kick".to_string()),
-        ("k".to_string(), "punch".to_string()),
-    ].iter().cloned().collect()
+fn count_occurrences(src: &str, target: &str) -> usize {
+    src.matches(target).count()
 }
 
-fn get_combos() -> IndexMap<Vec<String>, String> { //hardcode de l'enfer
-    [
-        (vec!["jump".to_string(), "kick".to_string()], "air kick".to_string()),
-        (vec!["jump".to_string(), "punch".to_string()], "air punch".to_string()),
-        (vec!["right".to_string(), "kick".to_string()], "big kick".to_string()),
-        (vec!["kick".to_string()], "fast kick".to_string()),
-    ].iter().cloned().collect()
+fn parse_keymap_line(line: &str) -> Option<(String, String)> {
+    let split_line: Vec<&str> = line.split(';').collect();
+    if count_occurrences(line, ";") == 1 {
+        Some((split_line[0].to_string(), split_line[1].to_string()))
+    } else {
+        println!("Parsing error: line '{}' in 'keymap' should contain one ';'", line);
+        exit(1)
+    }
 }
 
-pub fn parse_file(file: &String)-> (IndexMap<String, String>,IndexMap<Vec<String>, String>) {
-    let keys_values: Vec<&str> = file.split('\n').collect();
-    let keymap = get_keymap();
-    dbg!(&keymap);
-    let combos = get_combos();
-    dbg!(&combos);
-    (keymap, combos)
+fn get_keymap(file_keymap: &str) -> IndexMap<String, String> {
+    file_keymap.lines().filter_map(parse_keymap_line).collect()
+}
+
+
+// fn get_combos() -> IndexMap<Vec<String>, String> {
+
+// }
+
+fn check_separator(parts: &Vec<&str>) {
+    if parts.len() != 2 {
+        println!("Parsing error: file should contain one '\\n\\n'");
+        exit(1);
+    }
+}
+
+pub fn parse_file(file: &String) {
+    let parts: Vec<&str> = file.split("\n\n").collect();
+    check_separator(&parts);
+    // dbg!(&parts); // OK
+    let keymap = get_keymap(&parts[0]);
+    dbg!(&keymap); // OK
 }
