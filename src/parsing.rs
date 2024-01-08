c49719949ad1601373a3759f2e6558ba03cbdd0c
Sebastien LELEU <leleu.sebastien@hotmail.fr>
2024-01-08 19:44:42 +0100

    add get combos

--- src/parsing.rs
+++ src/parsing.rs
@@ -1,7 +1,6 @@
 use std::process::exit;
 use indexmap::IndexMap;
 
-
 fn count_occurrences(src: &str, target: &str) -> usize {
     src.matches(target).count()
 }
@@ -16,14 +15,25 @@ fn parse_keymap_line(line: &str) -> Option<(String, String)> {
     }
 }
 
+fn parse_combo_line(line: &str) -> Option<(Vec<String>, String)> {
+    let split_line: Vec<&str> = line.split(';').collect();
+    if count_occurrences(line, ";") == 1 {
+        let moves: Vec<String> = split_line[0].split('+').map(String::from).collect();
+        let combo = split_line[1].to_string();
+        Some((moves, combo))
+    } else {
+        println!("Parsing error: line '{}' in 'combos' should contain one ';'", line);
+        exit(1)
+    }
+}
+
 fn get_keymap(file_keymap: &str) -> IndexMap<String, String> {
     file_keymap.lines().filter_map(parse_keymap_line).collect()
 }
 
-
-// fn get_combos() -> IndexMap<Vec<String>, String> {
-
-// }
+fn get_combos(file_combos: &str) -> IndexMap<Vec<String>, String> {
+    file_combos.lines().filter_map(parse_combo_line).collect()
+}
 
 fn check_separator(parts: &Vec<&str>) {
     if parts.len() != 2 {
@@ -35,7 +45,9 @@ fn check_separator(parts: &Vec<&str>) {
 pub fn parse_file(file: &String) {
     let parts: Vec<&str> = file.split("\n\n").collect();
     check_separator(&parts);
-    // dbg!(&parts); // OK
+    dbg!(&parts); // OK
     let keymap = get_keymap(&parts[0]);
     dbg!(&keymap); // OK
+    let combos = get_combos(&parts[1]);
+    dbg!(&combos); // OK
 }
