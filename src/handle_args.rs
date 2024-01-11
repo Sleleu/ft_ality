ee450974f9cd85c368db9f559d9385e819ca3d2e
sleleu <leleu.sebastien@hotmail.fr>
2024-01-11 01:35:21 +0100

    fix file error when open

--- src/handle_args.rs
+++ src/handle_args.rs
@@ -1,8 +1,13 @@
 use std::{env, fs, process::exit};
 
-pub fn get_file(filename: &String)-> String {
-    fs::read_to_string(filename)
-        .expect("Error while reading file")
+pub fn get_file(filename: &str) -> String {
+    match fs::read_to_string(filename) {
+        Ok(file_str) => { file_str }
+        Err(e) => {
+            println!("Filename error: {}", e);
+            exit(1);
+        }
+    }
 }
 
 pub fn check_arg_len(len: usize) {
